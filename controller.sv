d34495f4ebb625c5fe0f1e417c93c3a8792e616f
m <197663917+Ali-TaHiR-090@users.noreply.github.com>
2025-05-06 11:31:03 +0500

    write code

--- controller.sv
+++ controller.sv
@@ -1,18 +1,30 @@
 module controller(
+    input logic clk,
     input logic command[11:0],
     output logic op_code[2:0],
 )
+    
 
+logic reset, enable, [7:0]q, [7:0]d, [7:0]reset, [7:0]enable;
+//instantiate the memory module
+memory mem[7:0](
+    .d(d),
+    .q(q),
+    .clk(clk),
+    .reset(reset),
+    .enable(enable)
+);
 //decode the commands
 instruction = command[11:9]
 addr1 = command[8:6]
 addr2 = command[5:3]
 addr3 = command[2:0]
 
-case (instruction)
+case (command) 
 
-
-    
 endcase
+
+
+
 
 endmodule
--- memory.sv
+++ memory.sv
@@ -1,3 +1,16 @@
-module memory()
+module memory(
+    input logic d[31:0], q[31:0],
+    input logic clk, reset, enable
+)
+always_ff @(posedge clk) begin
+    if (reset) begin
+        q <= 32'b0; // Reset the output to zero
+    end else if (enable) begin
+         q <= d; // Load data into the output on the rising edge of the clock
+    end else begin
+        q <= 0; // Maintain the current value if not enabled
+    end
+end
+
 
 endmodule
